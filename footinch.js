4db762e00d00c6579c46c0b8e2eb0f5092a2c6fe
Devanshu046 <dev44908@gmail.com>
2022-01-28 12:33:26 +0530

    footinch

---
+++ footinch.js
@@ -0,0 +1,19 @@
+let a=document.getElementById('t1');
+let b=document.getElementById('t2');
+let c=document.getElementById('t3');
+let d=document.getElementById('footinch');
+let e=document.getElementById('clr');
+
+d.addEventListener('click',footinch);
+e.addEventListener('click',clearall);
+
+function footinch(){
+
+    c.value=a.value%12;
+    b.value=(a.value)/12-(c.value/12);
+}
+function clearall(){
+    document.getElementById('t1').value='';
+    document.getElementById('t2').value='';
+    document.getElementById('t3').value='';
+}
