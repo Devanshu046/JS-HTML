c5a7aa55a2d8722a5a2af5b2e7e88accfdf3d264
Devanshu046 <dev44908@gmail.com>
2022-03-04 20:38:31 +0530

    js and html

---
+++ select.js
@@ -0,0 +1,18 @@
+let s1=document.getElementById('s1');
+let t1=document.getElementById('t1');
+let b1=document.getElementById('b1');
+let b2=document.getElementById('b2');
+let b3=document.getElementById('b3');
+b1.addEventListener('click',selectimg);
+b2.addEventListener('click',selecttext);
+b3.addEventListener('click',changeimg);
+
+function selectimg(){
+    s1.value=t1.value;
+}
+function selecttext(){
+    t1.value=s1.value;
+}
+function changeimg(){
+    download.jpg.setAttribute('src',"d1.jpg");
+}
