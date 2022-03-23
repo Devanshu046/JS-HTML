290dba28f0482c3771fd972a545db31994a7fa9e
Devanshu046 <dev44908@gmail.com>
2022-03-23 20:54:20 +0530

    js and html

--- trafficlights.js
+++ trafficlights.js
@@ -1,9 +1,26 @@
 let i1=document.getElementById('i1');
 let i2=document.getElementById('i2');
 let i3=document.getElementById('i3');
+let i4=document.getElementById('i4');
 let b1=document.getElementById('b1');
-b1.addEventListener('click',changeimg);
+let b2=document.getElementById('b2');
+b1.addEventListener('click',intervallight);
+b2.addEventListener('click',stop);
+let timeout;
+let a=["Yellow.PNG","Green.PNG","Red.PNG"];
+let i=0;
 
 function changeimg(){
-    i1.setAttribute('src',"download.jpg");
+    i4.setAttribute('src',a[i]);
+    //console.log(hello);
+    i++;
+    if(i==3){
+        i=0;
+    }
+}
+function intervallight(){
+    timeout=setInterval(changeimg,1000);
+}
+function stop(){
+    clearInterval(timeout);
 }
