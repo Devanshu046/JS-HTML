4fd565ba0bba95377307a5022ef833a66bcff988
Devanshu046 <dev44908@gmail.com>
2022-04-18 20:16:36 +0530

    js and html

--- onlydigit.js
+++ onlydigit.js
@@ -1,13 +1,24 @@
 let t1=document.getElementById('t1');
 t1.addEventListener('keydown',digit);
+let a=1;
 
 function digit(event){
+  console.log(event.key);
+  console.log(event);
+  console.log(event.key.toString().charCodeAt(0));
+  if(a==1 && (event.key.toString().charCodeAt(0))==46)
+  {
+    console.log(event.key.toString().charCodeAt(0));
+    a++; 
+  }
 
-  if((event.key.toString().charCodeAt(0))>=48 && (event.key.toString().charCodeAt(0))<=58)
+  else if((event.key.toString().charCodeAt(0))>=48 && (event.key.toString().charCodeAt(0))<=58) 
   {
     //console.log(t1.value);
     console.log(event.key.toString().charCodeAt(0));
   }
+  
+  
   else{
     event.preventDefault();
   }
